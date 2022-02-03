e2a058662682bf99a3f970d1accfc815bd80e742
XavierPerezz <xnator9@gmail.com>
2022-02-03 18:22:56 +0000

    adding task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string with printf
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
