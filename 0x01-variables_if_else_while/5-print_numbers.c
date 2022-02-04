1921d6dcc8d611d67297f0187245f96de02ca8ec
XavierPerezz <xnator9@gmail.com>
2022-02-04 19:53:51 +0000

    adding task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function to print numbers of base 10
+ *        followed by the ney line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int base;
+
+		for (base = 0 ; base < 10 ; base++)
+			printf("%d", base);
+
+		printf("\n");
+
+	return (0);
+}
