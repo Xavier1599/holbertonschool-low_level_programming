296991b96f40587b1b72d50b737dedc80549be2a
XavierPerezz <xnator9@gmail.com>
2022-02-08 17:15:30 +0000

    adding task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - using the custom header file to use putchar to print _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
+
+
+		return (0);
+
+}
