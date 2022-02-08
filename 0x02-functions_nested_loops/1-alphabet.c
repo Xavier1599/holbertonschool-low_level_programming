12aebd2cdc7bc0181475db8d6638fa9be00bcda8
XavierPerezz <xnator9@gmail.com>
2022-02-08 17:16:52 +0000

    adding task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - us putchar to print the alphabet
+ *
+ */
+
+void print_alphabet(void)
+{
+
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z'; alpha++)
+
+		_putchar(alpha);
+
+	_putchar('\n');
+
+}
