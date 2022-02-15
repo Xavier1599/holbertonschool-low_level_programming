5d04f6fdb534adf006574a0e6f07f36646fc8d5c
XavierPerezz <xnator9@gmail.com>
2022-02-15 23:39:58 +0000

    adding task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,14 +12,14 @@ void rev_string(char *s)
 	int i;
 	int store;
 
-	for (length = 0 ; s[length] != '\0'; ++length)
+	for (length = 0 ; s[length] != '\0'; length++)
 		;
 
-	for (i = 0; i < length - 1; i++)
-
+	for (i = 0; i < length / 2; i++)
+	{
 		store = s[i];
 	s[i] = s[length - i - 1];
 	s[length - i - 1] = store;
-
+	}
 
 }
