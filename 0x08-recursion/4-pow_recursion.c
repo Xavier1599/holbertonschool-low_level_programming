76d3168db233005df5fa9c2d6dbacb7943fecd4c
XavierPerezz <xnator9@gmail.com>
2022-02-24 17:38:21 +0000

    adding task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,13 +13,13 @@ int _pow_recursion(int x, int y)
 {
 	int res = x;
 
-	if (y < 0)
-		return (-1);
-
-	else if (y == 0)
+	if (y == 0)
 		return (1);
 
-	res += _pow_recursion(x, y - 1);
+	else if (y <= 0)
+		return (-1);
+
+	res *= _pow_recursion(x, y - 1);
 
 	return (res);
 }
