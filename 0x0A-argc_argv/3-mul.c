6aa20abd4eac9037b3adb9b4e96d646f23f479cb
XavierPerezz <xnator9@gmail.com>
2022-02-25 19:18:40 +0000

    adding task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ *
+ * @argc: counts the array of characters
+ * @argv: group of array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, res;
+
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	res = num1 * num2;
+
+	printf("%d\n", res);
+
+	return (0);
+
+}
