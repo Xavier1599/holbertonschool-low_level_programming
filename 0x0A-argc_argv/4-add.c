2158875c1b572573e0de5b4a51c0aefc4c8a915f
XavierPerezz <xnator9@gmail.com>
2022-02-25 21:21:08 +0000

    adding task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - program that adds positive numbers.
+ *
+ * @argc: count the array spaces
+ * @argv: string of aray
+ *
+ * Return: 1 if err0r and 0 if good
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int num, sum = 0;
+
+	if (argc > 1)
+	{
+
+		for (num = 1; num < argc; num++)
+		{
+
+			if (atoi(argv[num]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			else
+
+				sum += atoi(argv[num]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("0\n");
+
+	return (0);
+}
