0f8d1e7e2f14f4289337b75697bae30b0c801811
XavierPerezz <xnator9@gmail.com>
2022-03-01 21:28:06 +0000

    adding task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,12 @@ char *str_concat(char *s1, char *s2)
 	char *cat;
 	int idx, len = 0, concat = 0;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 
