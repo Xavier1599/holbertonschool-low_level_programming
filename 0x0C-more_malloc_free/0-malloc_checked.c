fd6735252e96164311734299da90d3f1d7d48f1c
XavierPerezz <xnator9@gmail.com>
2022-03-03 20:34:33 +0000

    adding task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,8 +3,11 @@
 
 
 /**
+ * malloc_checked - allocates memory using malloc.
  *
+ * @b: parameter to be checked
  *
+ * Return: check
  */
 
 void *malloc_checked(unsigned int b)
@@ -12,7 +15,7 @@ void *malloc_checked(unsigned int b)
 	int *check = malloc(b);
 
 	if (check == NULL)
-		exit (98);
+		exit(98);
 
 	return (check);
 
