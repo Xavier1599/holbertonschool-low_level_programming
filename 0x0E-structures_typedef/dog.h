cc4070c5c71e97ae96aed1fa0796bced6d6bcb8e
XavierPerezz <xnator9@gmail.com>
2022-03-07 19:13:16 +0000

    adding task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - defining a new struct with elements
+ *
+ * @name: a char parameter
+ * @age: a float parameter
+ * @owner: a char parameter
+ *
+ */
+
 struct dog
 {
 	char *name;
