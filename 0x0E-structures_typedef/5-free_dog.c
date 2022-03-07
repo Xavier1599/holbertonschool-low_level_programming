dfcf8361dc5ae78f03533bba75e8687330e551f0
XavierPerezz <xnator9@gmail.com>
2022-03-07 20:55:42 +0000

    adding task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * free_dog - frees dogs
+ *
+ * @d: parameter to free`
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
