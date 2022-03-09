e8ee44a58490571c167865ff9b9e348937861222
XavierPerezz <xnator9@gmail.com>
2022-03-09 18:36:04 +0000

    adding task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,12 @@
 
 
 /**
+ * print_name - function to print name
  *
+ * @name: parameter that holds the name
+ * @f: function pointer
  *
- *
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
