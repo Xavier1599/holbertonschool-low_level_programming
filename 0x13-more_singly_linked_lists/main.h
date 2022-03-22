2d6e8ca9acb09a37273551d12e7624c51e68b2b9
XavierPerezz <xnator9@gmail.com>
2022-03-22 17:20:54 +0000

    adding dir again

--- 0x13-more_singly_linked_lists/main.h
+++ 0x13-more_singly_linked_lists/main.h
@@ -16,8 +16,8 @@
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+		int n;
+			struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
@@ -34,4 +34,3 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
-
