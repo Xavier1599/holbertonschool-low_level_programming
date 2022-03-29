0114c650fd02ebebf542461b3bf298afc178e649
XavierPerezz <xnator9@gmail.com>
2022-03-29 18:58:53 +0000

    adding task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,51 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * main - program that copies the content of a file to another file.
- *
- * @ac: parameter
- * @av: parameter
- *
- * Return: 0
+ * main - copies text from one file to another
+ * @argc: arguments from command line
+ * @argv: command line string
+ * Return: 0 if succesful
  */
-int main(int ac, char **av)
+
+int main(int argc, char argv)
 {
-	int o_from, o_to, r = 1, w, close1, close2;
+	int fo_from, fo_to, fr = 1, fw, fc_from, fc_to;
 	char *buffer[1024];
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	o_from = open(av[1], O_RDONLY);
-	o_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	while (r > 0)
+	fo_from = open(argv[1], O_RDONLY);
+	fo_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+
+	while (fr > 0)
 	{
-		r = read(o_from, buffer, 1024);
-		if (o_from == -1 || r == -1)
+		fr = read(fo_from, buffer, 1024);
+		if (fo_from == -1  fr == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		w = write(o_to, buffer, r);
-		if (o_to == -1 || w == -1)
+
+		fw = write(fo_to, buffer, fr);
+		if (fo_to == -1  fw == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", av[2]);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	close1 = close(o_from);
-	close2 = close(o_to);
-	if (close1 == -1)
+	fc_from = close(fo_from);
+	fc_to = close(fo_to);
+	if (fc_from == -1)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", close1);
+		dprintf(2, "Error: Can't close fd %i\n", fo_from);
 		exit(100);
 	}
-	if (close2 == -1)
+	if (fc_to == -1)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", close2);
+		dprintf(2, "Error: Can't close fd %i\n", fo_to);
 		exit(100);
 	}
 	return (0);
