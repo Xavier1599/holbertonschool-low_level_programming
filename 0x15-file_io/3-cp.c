a32b29bef59e01f3a5689a2ab8e04f3be6c8b215
XavierPerezz <xnator9@gmail.com>
2022-03-29 18:47:59 +0000

    ading task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
  */
 int main(int ac, char **av)
 {
-	int o_from, o_to, r, w, close1, close2;
+	int o_from, o_to, r = 1, w, close1, close2;
 	char *buffer[1024];
 
 	if (ac != 3)
@@ -19,18 +19,21 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	o_from = open(av[1], O_RDONLY);
-	r = read(o_from, buffer, 1024);
-	if (o_from == -1 || r == -1)
+	o_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	while (r > 0)
 	{
-		dprintf(2, "Error: Can't read from file %s", av[1]);
-		exit(98);
-	}
-	o_to = open(av[2], O_CREAT | O_RDWR | O_APPEND, 0664);
-	w = write(o_to, buffer, r);
-	if (o_to == -1 || w == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[2]);
+		r = read(o_from, buffer, 1024);
+		if (o_from == -1 || r == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s", av[1]);
+			exit(98);
+		}
+		w = write(o_to, buffer, r);
+		if (o_to == -1 || w == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", av[2]);
 			exit(99);
+		}
 	}
 	close1 = close(o_from);
 	close2 = close(o_to);
