efead083e5cd189cd7520d7d1e649ff7c4630651
XavierPerezz <xnator9@gmail.com>
2022-03-29 16:30:57 +0000

    adding task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,33 +15,33 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	o_from = open(av[1], O_RDONLY);
 	r = read(o_from, buffer, 1024);
 	if (o_from == -1 || r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+		dprintf(2, "Error: Can't read from file %s", av[1]);
 		exit(98);
 	}
-	o_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	o_to = open(av[2], O_CREAT | O_RDWR | O_APPEND, 0664);
 	w = write(o_to, buffer, r);
 	if (o_to == -1 || w == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
+		dprintf(2, "Error: Can't read from file %s\n", av[2]);
 			exit(99);
 	}
 	close1 = close(o_from);
 	close2 = close(o_to);
 	if (close1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", close1);
+		dprintf(2, "Error: Can't close fd %i\n", close1);
 		exit(100);
 	}
 	if (close2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", close2);
+		dprintf(2, "Error: Can't close fd %i\n", close2);
 		exit(100);
 	}
 	return (0);
