98f61cfb49f61e06429432f3d91c4bafafe11235
XavierPerezz <xnator9@gmail.com>
2022-05-27 03:21:25 +0000

    adding task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ *
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int idx, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht ->size);
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+
+	return (1);
+}
