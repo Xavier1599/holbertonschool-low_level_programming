fd51eb2127e2307077810ecba2b7fc2905402ea1
XavierPerezz <xnator9@gmail.com>
2022-05-27 03:20:58 +0000

    fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULl)
+	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
